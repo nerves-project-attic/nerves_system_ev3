2f65f247939325eb2ae4a35ccf54f6a052a81650
Justin Schneck <jschneck@mac.com>
2016-04-02 22:08:48 -0400

    updated license and package info for hex

--- mix.exs
+++ mix.exs
@@ -3,21 +3,33 @@ defmodule NervesSystemEv3.Mixfile do
 
   def project do
     [app: :nerves_system_ev3,
-    version: "0.4.0-rc3",
-    elixir: "~> 1.2",
-    compilers: Mix.compilers ++ [:nerves_system],
-    deps: deps]
- end
+     version: "0.4.0-rc3",
+     elixir: "~> 1.2",
+     compilers: Mix.compilers ++ [:nerves_system],
+     description: description,
+     package: package,
+     deps: deps]
+  end
 
- def application do
+  def application do
    []
- end
+  end
 
- defp deps do
-   [
-     {:nerves_system, path: "../nerves_system"},
+  defp deps do
+    [{:nerves_system, path: "../nerves_system"},
      {:nerves_system_br, path: "../nerves-system-br"},
-     {:nerves_toolchain_arm_unknown_linux_gnueabihf, github: "nerves-project/nerves_toolchain_arm_unknown_linux_gnueabi"}
-   ]
- end
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, github: "nerves-project/nerves_toolchain_arm_unknown_linux_gnueabi"}]
+  end
+
+  defp description do
+    """
+    Nerves System - Lego Mindstorms EV3 http://www.lego.com/en-us/mindstorms/about-ev3
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_system_ev3"}]
+  end
 end
