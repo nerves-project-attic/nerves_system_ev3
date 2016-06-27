99367e327a28aae12c91957e782ac8fade70bdd3
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-27 11:23:34 -0400

    Add dev assets download

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,8 @@ config :nerves_system_ev3, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_ev3/releases/download/v#{version}/nerves_system_ev3-v#{version}.tar.gz"],
+    "https://github.com/nerves-project/nerves_system_ev3/releases/download/v#{version}/nerves_system_ev3-v#{version}.tar.gz",
+    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_ev3-#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
