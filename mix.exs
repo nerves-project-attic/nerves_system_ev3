4ae6fa438f10725211f8dd6b3ff07ff5ec45216b
Justin Schneck <jschneck@mac.com>
2016-09-14 20:12:07 -0400

    bump version and add 0.7.1 toolchain

--- mix.exs
+++ mix.exs
@@ -23,7 +23,14 @@ defmodule NervesSystemEv3.Mixfile do
     [{:nerves_system, "~> 0.1.4"},
       #{:nerves_system_br, "~> 0.6.1"},
      {:nerves_system_br, github: "nerves-project/nerves_system_br", tag: "a21bd9c6b3a907b8cbfdbe43194bf5cd33312607"},
-     {:nerves_toolchain_armv5tejl_unknown_linux_musleabi, "~> 0.7.0"}]
+     {:nerves_toolchain_armv5tejl_unknown_linux_musleabi,
+        github: "nerves-project/toolchains",
+        sparse: "nerves_toolchain_armv5tejl_unknown_linux_musleabi"},
+     {:nerves_toolchain_ctng,
+        github: "nerves-project/toolchains",
+        sparse: "nerves_toolchain_ctng",
+        override: true}
+    ]
   end
 
   defp description do
