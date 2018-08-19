dcb1cc631ec409660cbbfb38586ec40543bbeaf7
Justin Schneck <jschneck@mac.com>
2018-08-18 22:00:41 -0400

    Add provisioning.conf and include it in fwup.conf

--- post-build.sh
+++ post-build.sh
@@ -9,3 +9,6 @@ $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_D
 
 # Create the symlink to musl
 ln -sf /usr/lib/libc.so $TARGET_DIR/lib/ld-musl-arm.so.1
+
+# Copy the fwup includes to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
